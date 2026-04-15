f315ed93d9037b89b7229ce5989f5b2a178cfa46
Oleh Kazban <oleh.kazban@globallogic.com>
2026-04-15 21:13:12 +0300

    Init

---
+++ apps/employee/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { loadRemoteModule } from '@nx/angular/mf';
+
+export const appRoutes: Route[] = [
+  {
+    path: 'todo',
+    loadChildren: () =>
+      loadRemoteModule('todo', './Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      loadRemoteModule('login', './Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: '',
+    component: NxWelcomeComponent,
+  },
+];
